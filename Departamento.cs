b33691e132dee1d5ab1abde9e8f5ea57c640572a
Diego <Diego@LSOFT-PC>
2023-06-20 12:45:24 -0500

    Agregar archivos de proyecto.

---
+++ Departamento.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiendaBiblioteca
+{
+    public class Departamento
+    {
+        public string TipoProducto { get; set; }
+        public int Piso;
+        public Departamento() 
+        {
+            Console.Write("Ingrese el tipo de producto: ");
+            TipoProducto = Console.ReadLine();
+            Console.Write("Ingrese el piso donde se encuentra el departamento: ");
+            Piso = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
---
+++ Tienda.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace TiendaBiblioteca
+{
+    public class Tienda
+    {
+        public int Antigüedad { get; set; }  
+        public Departamento[] departamentos { get; set; }
+
+        public Tienda() 
+        {
+            Console.Write("Ingrese la Antigüedad de la tienda: ");
+            Antigüedad = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Cuántos departamentos va a tener de la tienda: ");
+            int CantDepartamentos = Convert.ToInt32(Console.ReadLine());
+
+            departamentos = new Departamento[CantDepartamentos];
+
+            for (int i = 0; i < departamentos.Length; i++)
+            {
+                departamentos[i] = new Departamento();
+            }
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine("Piso     Tipo de Producto");
+            Console.WriteLine("-------------------------");
+            for(int i = 0;i < departamentos.Length;i++)
+            {
+                Console.WriteLine(departamentos[i].Piso + "        " + departamentos[i].TipoProducto);
+
+            }
+        }
+    }
+}
